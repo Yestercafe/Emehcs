197542d6d0266bcb3bb63c4d933191b3f3365066
Ivan Chien <qyc027@gmail.com>
2022-03-20 22:23:16 +0800

    Remove WYaSi48H

--- WYaSi48H/ch1/ex2.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-import System.Environment
-
-main :: IO()
-main = do args <- getArgs
-          let a = read(args !! 0) :: Int
-          let b = read(args !! 1) :: Int
-          putStrLn (args !! 0 ++ " + " ++ args !! 1 ++ " = " ++ show(a + b))
